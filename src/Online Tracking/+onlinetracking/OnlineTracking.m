e96e2bb7477db25ec73ef4fdca9730d3ed54e218
arunniddish <63624213+arunniddish@users.noreply.github.com>
2026-01-25 19:47:02 -0600

    Refactor OnlineTracking class and improve tracking method

--- src/Online Tracking/+onlinetracking/OnlineTracking.m
+++ src/Online Tracking/+onlinetracking/OnlineTracking.m
@@ -1,4 +1,5 @@
 classdef OnlineTracking
+
     properties
         number_of_markers;
         centroids;
@@ -7,22 +8,21 @@ classdef OnlineTracking
         CurrPt;
         cent;
         tracking_data_centroid; % Added
-        
+
         vread;
         numberOfFrames;
         vwrite;
-        
+
         overlay_image;
         overlay;
-        stop_radius;
-        target_position;
-        obstaclesBB;
+
         robo_centroid;
-        trajectory_position;
         pixel_to_cm;
         robo_rotated;
     end
+
     methods
+
         function obj = OnlineTracking(params)
             obj.number_of_markers = params.number_of_markers;
             obj.PrevPt = [];
@@ -30,96 +30,96 @@ classdef OnlineTracking
             obj.CurrPt = [];
             obj.cent = [];
             obj.tracking_data_centroid = [];  % Added
-%             obj.vread = params.vread;
+            %             obj.vread = params.vread;
             obj.numberOfFrames = 1;
             obj.vwrite = params.vwrite_tracked;
             obj.centroids = zeros(obj.numberOfFrames,3*obj.number_of_markers);
-%             obj.overlay_image = params.overlay_img_cut;
+            %             obj.overlay_image = params.overlay_img_cut;
             obj.overlay = params.overlay;
             obj.stop_radius = params.stop_radius;
             obj.target_position = params.target_position;
             obj.obstaclesBB = params.obstaclesBB;
             obj.robo_centroid = [];
-            obj.trajectory_position = [];
+            % obj.trajectory_position = [];
             obj.pixel_to_cm = params.pixel_to_cm;
             obj.robo_rotated = [];
         end
 
-        function [output] = tracking(obj,thisFrame,PrevPt,P0,robo_centroid,trajectory_position,k)
-                
-                obj.PrevPt = PrevPt;
-                obj.P0 = P0;
-                obj.robo_centroid = robo_centroid;
-                obj.trajectory_position = trajectory_position;
-%                 thisFrame = read(obj.vread,k);
-                newim = createMaskhdblue(thisFrame);
-                newim = bwareaopen(newim,25);
-                newim = imfill(newim, 'holes');
+        function [output] = tracking(obj,thisFrame,PrevPt,P0,robo_centroid,k)
 
-                [labeledImage, numberOfRegions] = bwlabel(newim);
+            obj.PrevPt = PrevPt;
+            obj.P0 = P0;
+            obj.robo_centroid = robo_centroid;
+            % obj.trajectory_position = trajectory_position;
+            %                 thisFrame = read(obj.vread,k);
+            newim = createMaskhdblue(thisFrame);
+            newim = bwareaopen(newim,25);
+            newim = imfill(newim, 'holes');
 
-                count = 0;
-                obj.cent = zeros(numberOfRegions,2);
+            [labeledImage, numberOfRegions] = bwlabel(newim);
 
-                stats = regionprops(labeledImage, 'BoundingBox','Centroid','Area','EquivDiameter');
-                 for rb = 1:numberOfRegions
-                     count = count + 1;
-                     obj.cent(count,:) = stats(rb).Centroid;
-                     obj.cent(count,2) = 1080 - obj.cent(count,2) ;  % Correction for y-axis.
-                 end
+            count = 0;
+            obj.cent = zeros(numberOfRegions,2);
 
-%                  obj.tracking_data_centroid(k,:) = [mean(obj.cent(:,1)) mean(obj.cent(:,2))];   % Added ANM
-%                  tc(k,:) = obj.tracking_data_centroid(k,:);     
-             
-                  zc = zeros(size(obj.cent,1),1);
-                  obj.cent = [obj.cent,zc];
+            stats = regionprops(labeledImage, 'BoundingBox','Centroid','Area','EquivDiameter');
+            for rb = 1:numberOfRegions
+                count = count + 1;
+                obj.cent(count,:) = stats(rb).Centroid;
+                obj.cent(count,2) = 1080 - obj.cent(count,2) ;  % Correction for y-axis.
+            end
 
-                  if k == 1
-                    obj.P0 = obj.cent;
-                    output.P0 = obj.P0;
-                    obj.PrevPt = obj.cent;
-                    output.PrevPt = obj.PrevPt;
-                    obj.centroids = data_logging(obj,k);
-                    theta = zeros(1,9);
-                    trans = zeros(1,3);
-                    theta_G = zeros(1,9);
-                    trans_G = zeros(1,3);
-                    obj.robo_centroid(k,:) = [mean(obj.PrevPt(:,1)) mean(obj.PrevPt(:,2))];
-                    output.robo_centroid = obj.robo_centroid;
-                    output.robo_rotated = 0;
-                    obj.robo_rotated = output.robo_rotated;
-                    plot(obj,thisFrame,count,trajectory_position,k);
-                  end
+            %                  obj.tracking_data_centroid(k,:) = [mean(obj.cent(:,1)) mean(obj.cent(:,2))];   % Added ANM
+            %                  tc(k,:) = obj.tracking_data_centroid(k,:);
 
-                  if k ~= 1
+            zc = zeros(size(obj.cent,1),1);
+            obj.cent = [obj.cent,zc];
 
-                     obj.CurrPt = nearest_neighbor(obj,count);                    
+            if k == 1
+                obj.P0 = obj.cent;
+                output.P0 = obj.P0;
+                obj.PrevPt = obj.cent;
+                output.PrevPt = obj.PrevPt;
+                obj.centroids = data_logging(obj,k);
+                theta = zeros(1,9);
+                trans = zeros(1,3);
+                theta_G = zeros(1,9);
+                trans_G = zeros(1,3);
+                obj.robo_centroid(k,:) = [mean(obj.PrevPt(:,1)) mean(obj.PrevPt(:,2))];
+                output.robo_centroid = obj.robo_centroid;
+                output.robo_rotated = 0;
+                obj.robo_rotated = output.robo_rotated;
+                plot(obj,thisFrame,count,k);
+            end
 
-                     [Rot,T] = pose_estimation(obj,obj.CurrPt,obj.PrevPt,k);
-                     theta(k,:) = reshape(Rot,[1,9]);
-                     trans(k,:) = T';
+            if k ~= 1
 
-                     [Rot,T] = pose_estimation(obj,obj.P0,obj.CurrPt,k);
-                     theta_G(k,:) = reshape(Rot,[1,9]);
-                     trans_G(k,:) = T';
-                     output.robo_rotated = rotm2eul(Rot);
+                obj.CurrPt = nearest_neighbor(obj,count);
 
-                     obj.PrevPt = obj.CurrPt;
-                     obj.centroids = data_logging(obj,k);
-                     
-                     obj.robo_centroid(k,:) = [mean(obj.PrevPt(:,1)) mean(obj.PrevPt(:,2))];
-                     output.robo_centroid = obj.robo_centroid;
-                     obj.robo_rotated = output.robo_rotated;
-                     plot(obj,thisFrame,count,trajectory_position,k);
+                [Rot,T] = pose_estimation(obj,obj.CurrPt,obj.PrevPt,k);
+                theta(k,:) = reshape(Rot,[1,9]);
+                trans(k,:) = T';
 
-                  end
+                [Rot,T] = pose_estimation(obj,obj.P0,obj.CurrPt,k);
+                theta_G(k,:) = reshape(Rot,[1,9]);
+                trans_G(k,:) = T';
+                output.robo_rotated = rotm2eul(Rot);
+
+                obj.PrevPt = obj.CurrPt;
+                obj.centroids = data_logging(obj,k);
+
+                obj.robo_centroid(k,:) = [mean(obj.PrevPt(:,1)) mean(obj.PrevPt(:,2))];
+                output.robo_centroid = obj.robo_centroid;
+                obj.robo_rotated = output.robo_rotated;
+                plot(obj,thisFrame,count,k);
+
+            end
 
             output.tracking_data = cat(2,obj.centroids,theta,trans,theta_G,trans_G);
-%             close(obj.vwrite);
+            %             close(obj.vwrite);
         end
 
         function centroids = nearest_neighbor(obj,count)      %Change the name of the centroid ->
-            
+
             obj.CurrPt = zeros(obj.number_of_markers,3);
             if(count > obj.number_of_markers)
                 for i = 1:obj.number_of_markers
@@ -127,7 +127,7 @@ classdef OnlineTracking
                         X = [obj.PrevPt(i,:);obj.cent(j,:)];
                         d(j) = pdist(X,'euclidean');
                     end
-                    [dmin,ind] = min(d);  
+                    [dmin,ind] = min(d);
                     if(dmin < 15)
                         obj.CurrPt(i,:) = obj.cent(ind,:);
                     end
@@ -145,12 +145,12 @@ classdef OnlineTracking
                     end
                 end
             end
-            
+
             clear d;
             TF = obj.CurrPt(:,1);  % Writing the 1st column of resrvd
             index = find(TF == 0);  % Finding those rows which is empty
-            val = isempty(index);  % Checking whether the index is empty  
-            
+            val = isempty(index);  % Checking whether the index is empty
+
             if(val == 0)
                 centroids = occlusion(obj,index);   %Change the name of the centroid ->
             end
@@ -158,14 +158,14 @@ classdef OnlineTracking
                 centroids = obj.CurrPt;       %Change the name of the centroid ->
             end
 
-        end 
+        end
 
         function centroids = occlusion(obj,index)   %Change the name of the centroid ->
             newPrevPt = obj.PrevPt;
-            newP0 = obj.P0; 
+            newP0 = obj.P0;
             newPrevPt(index(1:size(index,1)),:) = 0;
             newP0(index(1:size(index,1)),:) = 0;
-%             [Rot,T,~,~] = pose_estimation(newPrevPt,obj.CurrPt); % SE2 w.r.t previous frame
+            %             [Rot,T,~,~] = pose_estimation(newPrevPt,obj.CurrPt); % SE2 w.r.t previous frame
             [Rot,T] = pose_estimation(obj,newPrevPt,obj.CurrPt); % SE2 w.r.t previous frame
             for gg = 1:size(index,1)
                 newPt = Rot*(obj.PrevPt(index(gg),:))' + T;
@@ -186,75 +186,67 @@ classdef OnlineTracking
         function [Rot,T,theta,trans] = pose_estimation(obj,A,B,k)
 
             [Rot,T] = rigid_transform_3D(A',B');  % SE2 w.r.t previous frame
-%             theta(k,:) = reshape(Rot,[1,9]);   % Changed ANM
-%             trans(k,:) = T';
+            %             theta(k,:) = reshape(Rot,[1,9]);   % Changed ANM
+            %             trans(k,:) = T';
 
         end
 
-        function plot(obj,thisFrame,count,trajectory_position,k)
+        function plot(obj,thisFrame,count,k)
 
             figure(10)
             imshow(thisFrame)
-            set(gcf, 'Position',  [100, 100, 1000, 1000])
+            set(gcf, 'Position',  [100, 100, 1080, 1080])
             hold on
-            
+
             %Plot markers on the robot
             plot(obj.PrevPt(:,1),1080-obj.PrevPt(:,2),'g*','LineWidth',0.5,'MarkerSize',2)
-%             plot(obj.tracking_data_centroids(k,1),1080-obj.tracking_data_centroid(k,2),'g*','LineWidth',0.5,'MarkerSize',2)
-     
+            %             plot(obj.tracking_data_centroids(k,1),1080-obj.tracking_data_centroid(k,2),'g*','LineWidth',0.5,'MarkerSize',2)
+
             %Plot centroid of the robot
             plot(obj.robo_centroid(:,1),1080-obj.robo_centroid(:,2),'c*','LineWidth',2.5,'MarkerSize',2)
-            
-            %Plot path planner trajectory
-%             plot(obj.trajectory_position(1,:)*obj.pixel_to_cm,1080-(obj.trajectory_position(2,:)*obj.pixel_to_cm),'-r','LineWidth',2);
-            plot(trajectory_position(1,:)*obj.pixel_to_cm,1080-(trajectory_position(2,:)*obj.pixel_to_cm),'-r','LineWidth',2);
-            
+
             %Plot quiver
-%             plot(obj.robo_centroid(k,1),1080-obj.robo_centroid(k,2),40*cos(obj.robo_rotated),10*sin(obj.robo_rotated),'LineWidth',2,'Color',"#D95319");
-            
-            
+            %             plot(obj.robo_centroid(k,1),1080-obj.robo_centroid(k,2),40*cos(obj.robo_rotated),10*sin(obj.robo_rotated),'LineWidth',2,'Color',"#D95319");
+
+
             % Bounding box
-            for ii = 1:size(obj.obstaclesBB,1)
-               h = rectangle('Position',obj.obstaclesBB(ii,:),'EdgeColor','y','LineWidth',3);
-            end
-            
-            % Target/Goal position
-            plot(obj.target_position(:,1),1080-obj.target_position(:,2),'r*','LineWidth',0.7,'MarkerSize',5)
-            viscircles([obj.target_position(:,1) 1080-obj.target_position(:,2)],obj.stop_radius,'LineStyle','--');
+            % for ii = 1:size(obj.obstaclesBB,1)
+            %     h = rectangle('Position',obj.obstaclesBB(ii,:),'EdgeColor','y','LineWidth',3);
+            % end
 
             caption = sprintf('%d blobs found in frame %d', count, k);
             title(caption, 'FontSize', 20);
-            
+
             %Legends
-            legendnames = {'Robot markers','Robot path traced','Path Planner Path','Goal position'};
-            legend(legendnames);
-            
+            % legendnames = {'Robot markers','Robot path traced','Path Planner Path','Goal position'};
+            % legend(legendnames);
+
             axis on;
             hold off
             pframe = getframe(gcf);
             writeVideo(obj.vwrite,pframe);
 
-        end  
-%         
-%         function plot(obj,thisFrame,count,k)
-% % 
-%             figure(10)
-%             imshow(thisFrame)
-%             set(gcf, 'Position',  [100, 100, 1000, 1000])
-%             hold on
-% %             plot(obj.PrevPt(k,1),1080-obj.PrevPt(k,2),'g*','LineWidth',0.5,'MarkerSize',2)
-%             plot(obj.tracking_data_centroid(k,1),1080-obj.tracking_data_centroid(k,2),'g*','LineWidth',0.5,'MarkerSize',2)
-%             caption = sprintf('%d blobs found in frame #%d 0f %d', count, k, obj.numberOfFrames);
-%             title(caption, 'FontSize', 20);
-%             axis on;
-% %             hold off
-%             ovly_img_hdl = imagesc(obj.overlay_image);              % overlay (reference) image
-%             set(ovly_img_hdl, 'AlphaData', 0.4);    % set overlaid image alpha
-%             hold off;
-%             pframe = getframe(gcf);
-%             writeVideo(obj.vwrite,pframe);
-% 
-%         end 
+        end
+        %
+        %         function plot(obj,thisFrame,count,k)
+        % %
+        %             figure(10)
+        %             imshow(thisFrame)
+        %             set(gcf, 'Position',  [100, 100, 1000, 1000])
+        %             hold on
+        % %             plot(obj.PrevPt(k,1),1080-obj.PrevPt(k,2),'g*','LineWidth',0.5,'MarkerSize',2)
+        %             plot(obj.tracking_data_centroid(k,1),1080-obj.tracking_data_centroid(k,2),'g*','LineWidth',0.5,'MarkerSize',2)
+        %             caption = sprintf('%d blobs found in frame #%d 0f %d', count, k, obj.numberOfFrames);
+        %             title(caption, 'FontSize', 20);
+        %             axis on;
+        % %             hold off
+        %             ovly_img_hdl = imagesc(obj.overlay_image);              % overlay (reference) image
+        %             set(ovly_img_hdl, 'AlphaData', 0.4);    % set overlaid image alpha
+        %             hold off;
+        %             pframe = getframe(gcf);
+        %             writeVideo(obj.vwrite,pframe);
+        %
+        %         end
 
     end
 
