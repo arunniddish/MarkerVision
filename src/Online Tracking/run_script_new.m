eefa945e3277af3438f17cde5c1fe1e206ead267
arunniddish <63624213+arunniddish@users.noreply.github.com>
2026-01-25 20:03:10 -0600

    Enhance documentation in run_script_new.m
    Added detailed comments and documentation for the script, including inputs, outputs, and descriptions of the tracking data.

--- src/Online Tracking/run_script_new.m
+++ src/Online Tracking/run_script_new.m
@@ -1,3 +1,38 @@
+%========================================================================================
+% Script Name: run_script.m
+% Author: Arun Niddish Mahendran
+% Last modified date: 2024-MM-DD
+% Description: Briefly describe what this script does.
+% Inputs:
+% 1) "input_vid_dir": Path to directory of the video to be
+%                     tracked.
+%
+% 2) "vid_name"     : Video's name with the extension to be
+%                     tracked.
+% 
+% 3) "output_vid_filename" : Specify any name with the extension
+%                            to save the animation of tracked
+%                            file
+% 4) "params.number_of_markers" : Number of markers on the body 
+%                                 to be tracked
+%
+% Outputs: 
+% 1) "tracking_data" : Centroid data (x,y,z) of the respective markers on the robot,
+%                      global rotation and translation matrix; body
+%                      rotation and translation matrix.
+%
+% The data are in the following order:
+% 1 to 3 [1x3] Marker 1 [x,y,z]
+% 4 to 6 [1x3] Marker 2 [x,y,z]
+% 7 to 9 [1x3] Marker 3 [x,y,z]
+% 10 to 12 [1x3] Marker 4 [x,y,z]
+% 13 to 21 [1x9] Rotation matrix Intermediate frames [Reshaped from 3x3 to 1x9]
+% 22 to 24 [1x3] Translation Matrix Intermediate frames [[x,y,z]Reshaped from 3x1 to 1x3]
+% 25 to 33 [1x9] Rotation matrix Global frame [Reshaped from 3x3 to 1x9]
+% 34 to 36 [1x3] Translation Matrix Global frame [[x,y,z]Reshaped from 3x1 to 1x3]
+% 37 [1x1] Timestamp
+%=========================================================================================
+
 clear all;
 clc;
 
